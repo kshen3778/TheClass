9937129d65861e1829f776d45a501e17efeb2493
splacorn <curtischong5@gmail.com>
2015-04-23 18:19:39 -0400

    Revert "login page stuff"
    This reverts commit 278fc835e506eb0449343ea0689206e2cbd08c9b.

--- account settings page/accountsettings-script.js
+++ account settings page/accountsettings-script.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-    $("#logintab").hide()
     $("#openlogin").on("click",function(){
-        $("#openlogin").animate({ top: '+=100px'}, { duration: 500, queue: false });
-        $("#openlogin").fadeTo(500, 0);
-        $("#logintab").animate({ top: '+=200px'}, { duration: 500, queue: false });
-        $("#logintab").fadeTo(500, 1);
+        alert("hi");
     });
 });
