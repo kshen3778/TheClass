c22d99509af7daed4c9a2e9ebd9f4f103c47bf8c
splacorn <curtischong5@gmail.com>
2015-04-23 19:15:17 -0400

    dsf

---
+++ main page/mainpage-script.js
@@ -0,0 +1,11 @@
+alert("hi");
+$(document).ready(function () {
+    alert("test");
+    $("#logintab").hide();
+    $("#openlogin").on("click", function () {
+        $("#openlogin").animate({ top: '+=100px'}, { duration: 500, queue: false });
+        $("#openlogin").fadeTo(500, 0);
+        $("#logintab").animate({ top: '+=200px'}, { duration: 500, queue: false });
+        $("#logintab").fadeTo(500, 1);
+    });
+});
