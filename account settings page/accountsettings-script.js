278fc835e506eb0449343ea0689206e2cbd08c9b
splacorn <curtischong5@gmail.com>
2015-04-23 18:03:38 -0400

    login page stuff

--- account settings page/accountsettings-script.js
+++ account settings page/accountsettings-script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
+    $("#logintab").hide()
     $("#openlogin").on("click",function(){
-        alert("hi");
+        $("#openlogin").animate({ top: '+=100px'}, { duration: 500, queue: false });
+        $("#openlogin").fadeTo(500, 0);
+        $("#logintab").animate({ top: '+=200px'}, { duration: 500, queue: false });
+        $("#logintab").fadeTo(500, 1);
     });
 });
