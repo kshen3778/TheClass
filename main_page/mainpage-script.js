fbc6b78a3eb300ed2c7c5339f4adc2cc36163653
splacorn <curtischong5@gmail.com>
2015-04-24 18:12:42 -0400

    reg and log is done

--- main_page/mainpage-script.js
+++ main_page/mainpage-script.js
@@ -1,9 +1,40 @@
 $(document).ready(function() {
-    $("#logintab").hide()
+    $("#logintab").hide();
+    $("#registrationtab").hide();
+    
     $("#openlogin").on("click",function(){
         $("#openlogin").animate({ top: '+=100px'}, { duration: 500, queue: false });
         $("#openlogin").fadeTo(500, 0);
         $("#logintab").animate({ top: '+=200px'}, { duration: 500, queue: false });
         $("#logintab").fadeTo(500, 1);
+        $("#register").hide();
+    });
+    
+        $("#register").on("click",function(){
+        $("#register").animate({ top: '+=100px'}, { duration: 500, queue: false });
+        $("#register").fadeTo(500, 0);
+        $("#registrationtab").animate({ top: '+=200px'}, { duration: 500, queue: false });
+        $("#registrationtab").fadeTo(500, 1);
+        $("#openlogin").fadeTo(500, 0);
+    });
+    $("#closetablog").on("click",function(){
+        $("#logintab").animate({ top: '+100px' }, { duration: 500, queue: false });
+        $("#logintab").fadeTo(500, 0,function(){
+        $("#logintab").hide();
+        });
+        $("#openlogin").animate({ top: '+400px' }, { duration: 500, queue: false });
+        $("#openlogin").fadeTo(500, 1,function(){
+            $("#register").fadeTo(500, 1);
+        });
+    });
+    $("#closetabreg").on("click",function(){
+        $("#registrationtab").animate({ top: '+100px'}, { duration: 500, queue: false });
+        $("#registrationtab").fadeTo(500, 0,function(){
+        $("#registrationtab").hide();
+        });
+        $("#register").animate({ top: '+440px'}, { duration: 500, queue: false });
+        $("#register").fadeTo(500, 1,function(){
+        $("#openlogin").fadeTo(500, 1);
+        });
     });
 });
