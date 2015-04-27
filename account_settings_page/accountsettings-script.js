1ba26946febaefe8259fa5b0609f745d00e90570
splacorn <curtischong5@gmail.com>
2015-04-26 20:45:14 -0400

    made you able to edit your personal info
    test
    test
    test
    
    sd
    ad
    gds
    cf
    dx
    fds
    f

--- account_settings_page/accountsettings-script.js
+++ account_settings_page/accountsettings-script.js
@@ -1,6 +1,53 @@
-$(document).ready(function() {
-$("#aboutyou").hide();
-    $("#aboutyoupar").on("click",function(){
-        alert("hi");
+$(document).ready(function(){
+    $(".write").hide();
+    var hideeditbuttons = function(){
+        $("#cancelchangeinfo").hide();
+        $("#savechanges").hide();
+    };
+    hideeditbuttons();
+    var resetchangeinfo = function(){
+        $(".write").hide();
+        $('.edit').show();
+    };
+    var resetchangecomple = function(){
+        resetchangeinfo();
+        hideeditbuttons();
+    };
+    var ele;
+    $('.edit').on("click",function(event){
+        resetchangeinfo();
+        ele = event.target;
+        $(ele).hide();
+        $(ele).siblings(".write").show();
+        $("#cancelchangeinfo").show();
+        $("#savechanges").show();
+        $(ele).siblings(".personaldata").hide();
+        var replacepertext = $(ele).siblings(".personaldata");
+        $(ele).siblings(".write").val(replacepertext.html());
     });
+    
+    $("#savechanges").on("click",function(){
+        $(ele).siblings(".personaldata").remove();
+    var perdata = $("#yourinterests").val();
+        perdata = perdata + $("#aboutyou").val();
+        $(ele).after('<div class="personaldata">'+perdata+'</div>');
+    $('#yourinterests, #aboutyou').val("");
+        resetchangecomple();
+        $(".personaldata").show();
+    });
+    $("#cancelchangeinfo").on("click",function(){
+            resetchangecomple();
+        $(".personaldata").show();
+    });
+    
+    
+    /*$(document).mouseup(function(e){
+        var container = $(".write, #personalinfo, .unchangeinfo");
+        if (!container.is(e.target)){
+            $(".write").hide();
+            $(".edit").show();
+        }
+    });*/
+    
+    //javascript:document.body.contentEditable='true';
 });
