d19d3875c7f19ecdd636283e9ab84579e0e29107
splacorn <curtischong5@gmail.com>
2015-04-30 22:29:52 -0400

    added subtabs for dashboard and a lot of other stuff

--- home_page/homepage-script.js
+++ home_page/homepage-script.js
@@ -3,20 +3,26 @@ $(document).ready(function() {
 
     $("#navprofile").on("click",function(){
         if(toggleshowprofile==true){
-            $("#profilesettingcon").stop().animate({left: "+=200px"});
+            $("#profilesettingcon").animate({left: "+=200px"});
             toggleshowprofile=false;
         }
         else{
-            $("#profilesettingcon").stop().animate({left: "-=200px"});
+            $("#profilesettingcon").animate({left: "-=200px"});
             toggleshowprofile = true;
         }
     });
+    $("#dashboardtabcontainer").hide();
+    $("#dashboardtabcontainer").fadeIn(1000);
     $("#browse").hide();
     $("#dashboardtab").on("click",function(){
+        $("#dashboardtabcontainer").fadeIn(1000);
+        $("#dashboardtab").animate({width: '300px'});
         $("#dashboard").show();
         $("#browse").hide();
     });
     $("#browsetab").on("click",function(){
+        $("#dashboardtabcontainer").hide();
+        $("#dashboardtab").animate({width: '100px'});
         $("#dashboard").hide();
         $("#browse").show();
     });
@@ -24,18 +30,39 @@ $(document).ready(function() {
     $("#create").on("click",function(){
         $("#htmlpage").fadeTo(500,0.5);
         $("#navbar").fadeTo(500,0.5);
-        $("#htmlpage").css("top","-16.9vh");
+        $("#htmlpage").css("top","-168px");
         $("#createpopupbox").fadeIn();
         $("#htmlpage").css("z-index","-1");
+        $("#navbar").css("z-index","-1");
     });
     $("#closecreate").on("click",function(){
         $("#htmlpage").fadeTo(500,1);
         $("#navbar").fadeTo(500,1);
         $("#createpopupbox").fadeOut(function(){
-            $("#htmlpage").css("top","4.1vh");
+            $("#htmlpage").css("top","32px");
             $("#htmlpage").css("z-index","0");
+            $("#navbar").css("z-index","1");
         });
     });
+    
+    $("#dashboardyourcourses").on("click",function(){
+        $("#dashboardyourcourses").css({"height":"20px","border-bottom":"none"});
+        $("#dashboardyourlikedcontent").css({"height":"19px","border-bottom":"solid black 1px"});
+        $("#dashboardyoursubs").css({"height":"19px","border-bottom":"solid black 1px"});
+    });
+    $("#dashboardyourlikedcontent").on("click",function(){
+        $("#dashboardyourlikedcontent").css({"height":"20px","border-bottom":"none"});
+        $("#dashboardyoursubs").css({"height":"19px","border-bottom":"solid black 1px"});
+        $("#dashboardyourcourses").css({"height":"19px","border-bottom":"solid black 1px"});
+    });
+    $("#dashboardyoursubs").on("click",function(){
+        $("#dashboardyoursubs").css({"height":"20px","border-bottom":"none"});
+        $("#dashboardyourcourses").css({"height":"19px","border-bottom":"solid black 1px"});
+        $("#dashboardyourlikedcontent").css({"height":"19px","border-bottom":"solid black 1px"});
+    });
+    
+    
+    
     
     
     });
