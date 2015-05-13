2fb5770d2db29dd3cdbecda933b5630db16d1215
splacorn <curtischong5@gmail.com>
2015-05-12 22:42:32 -0400

    daily sync

--- create_resource_page/createresources-script.js
+++ create_resource_page/createresources-script.js
@@ -29,14 +29,28 @@ $(document).ready(function() {
         $("#navbar").fadeTo(500,1);
         $("#navbarinfo").fadeTo(500,1);
         $("#createpopupbox").fadeOut(function(){
-            $("#htmlpage").css("top","-8px");
-            $("#navbarinfo").css("top","-8px")
-            $("#htmlpage").css("z-index","0");
-            $("#navbar").css("z-index","1");
-            $("#navbarinfo").css("z-index","1");
+            $("#htmlpage").css("top", "-8px");
+            $("#navbarinfo").css("top", "-8px");
+            $("#htmlpage").css("z-index", "0");
+            $("#navbar").css("z-index", "1");
+            $("#navbarinfo").css("z-index", "1");
         });
     });
     //end of navbar js
-    
+    $("#addcircle").on("click",function() {
+        $("#createordeleteresourcecon").animate({top: "+=600px"},"slow");
+        $("#conclusioncon").animate({top: "+=600px"},"slow");
+        $("#htmlpage").animate({height: "+=600px"},"slow",function(){
+            $("#createordeleteresourcecon").before('<div class="resourcecontainer"><div class="uploadresource"></div><textarea class="resourcedescription" maxlength="800" placeholder="describe how it should be used in the classroom"></textarea></div>');
+        });
+    });
 
+    
+    
+    
+        /*$("#deletecircle").on("click",function(event){
+            if(confirm("delete resource?")==true){
+                $(event.target).closest('.resourcecontainer').remove();
+            }
+        });*/
     });
