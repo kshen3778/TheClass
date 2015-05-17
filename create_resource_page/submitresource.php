da4fdc44efbc44a261ab62c50225479822ab9319
kshen3778 <kshen3778@gmail.com>
2015-05-17 14:09:00 -0400

    Encoding Lesson info in JSON 2

--- create_resource_page/submitresource.php
+++ create_resource_page/submitresource.php
@@ -1,15 +1,23 @@
 <?php
+session_start();
+
 $title = $_POST['title'];
 $takeaway = $_POST['takeaway'];
 $intro = $_POST['intro'];
 $conclusion = $_POST['conclusion'];
 $files = $_POST['files'];
 
-$data = array();
-$data[] = ['title' => $title, 'takeaway' => $takeaway, 'intro' => $intro, 'conclusion' => $conclusion, 'files' => $files];
+$data = array('title' => $title, 
+	'takeaway' => $takeaway,
+	'intro' => $intro, 
+	'conclusion' => $conclusion, 
+	'files' => $files
+);
+//$data[] = ['title' => $title, 'takeaway' => $takeaway, 'intro' => $intro, 'conclusion' => $conclusion, 'files' => $files];
+
 //generate the json file and copy data to it
-/*$fp = fopen('results.json', 'w');
-fwrite($fp, json_encode($response));
-fclose($fp);*/
+$fp = fopen($_SESSION['path'] . 'data.json', 'w');
+fwrite($fp, json_encode($data));
+fclose($fp);
 
 ?>
