cb0c47d9767864944f9ead379293b0186401c6ea
splacorn <curtischong5@gmail.com>
2015-05-21 18:26:11 -0400

    made seect only one type of reading availibel

---
+++ lesson_creator_page/lessoncreator-index.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+?>
+<!DOCTYPE HTML>
+<html>
+	<link rel="stylesheet" type="text/css" href="lessoncreator-stylesheet.css"/>
+    	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script type='text/javascript' src='lessoncreator-script.js'></script>
+    
+		<head>
+		<title>test</title>
+		</head>
+    <!--<div id="createpopupbox">-->
+    <div id="createpopupbox" class="center">
+            <div id="closecreate">
+        <div class="xbutton">x</div>
+            </div>
+                <div id="resourcelessoncreate">resource/lesson</div>
+                <div id="coursecreate">course</div>
+    </div>
+<body>
+    <navbar id="navbar"></navbar>
+    <div id="navbarinfo" class="center">
+    <input id="generalsearch" type="text" placeholder="search" style="padding-left: 5px;" title="browse for lessons and resources">
+        
+                <div id="hyperlinkbuttons">
+            <div id="browsebutton">Browse</div>
+            <div id="homepagelink" onclick="location.href = '../home_page/homepage-index.php';">Home Page</div>
+            <div id="create">Create</div>
+            <div id="forum">Forum</div>
+        </div>
+        
+        
+            <div id="navprofile"><?php echo $_SESSION['username'];?></div>
+                <div id="profilesettingcon">
+                    <div id="accountsettings" onclick="location.href = '../account_settings_page/accountsettings-index.php';">profile</div>
+                    <div id="logout" onclick="location.href = '../Login_Registration/logout.php';">logout</div>
+                </div>
+    </div>
+    <div id="htmlpage" class="center">
+        <input id="lessontitle" placeholder="title of your lesson">
+        <textarea id="lessondescription" placeholder="your lesson description" maxlength="800"></textarea>
+        
+        
+        
+        <div id="submitsupportingcontent"> Submit Supporting Content</div>
+        	<form id="confirmresourceform" enctype="multipart/form-data" action="upload.php" method="post" style="display: none;">
+		<input id="uploadsupportingcontent" name="fileToUpload" type="file" value="Upload Resources"/>
+		<input id="confirmresource" type="submit" name="confirmresource">
+	</form>
+        <div id="uploadtextareaorreading">
+            <div id="insertreading"></div>
+        <textarea id="writenlecture"></textarea>
+        </div>
+        <div id="submitchanges">Submit Changes</div>
+    </div>
+</body>
+</html>
---
+++ lesson_creator_page/lessoncreator-script.js
@@ -0,0 +1,100 @@
+var toggleshowprofile = false;
+$(document).ready(function() {
+//navbar js
+    $("#navprofile").on("click",function(){
+        if(toggleshowprofile==true){
+            $("#profilesettingcon").animate({left: "+=200px"});
+            toggleshowprofile=false;
+        }
+        else{
+            $("#profilesettingcon").animate({left: "-=200px"});
+            toggleshowprofile = true;
+        }
+    });
+	
+	//click to go to profile page
+	/*$("#accountsettings").on("click",function(){
+         //redirect to account settings/profile page
+    });*/
+	
+	//logout: call logout script
+	$("#logout").on('click', function(){
+		var username = document.getElementById('username').value;
+		if(document.forms['form']['username'].value != ""){
+			console.log("forgotpass function is running");
+			$.ajax({
+			   type: "POST",
+			   url: 'forgotpass.php',
+			   data: { name: username},
+			   success: function(data){
+					console.log(data);
+			   }
+			});
+		}else{
+			alert("Enter username first");
+		}
+    })
+	
+    $("#createpopupbox").hide();
+    /*$("#create").on("click",function(){
+        $("#htmlpage").fadeTo(500,0.5);
+        $("#navbar").fadeTo(500,0.5);
+        $("#navbarinfo").fadeTo(500,0.5);
+        $("#htmlpage").css("top","-208px");
+        $("#navbarinfo").css("top","-208px")
+        $("#createpopupbox").fadeIn();
+        $("#htmlpage").css("z-index","-1");
+        $("#navbar").css("z-index","-1");
+        $("#navbarinfo").css("z-index","-1");
+        //$('html, body').animate({ scrollTop: 0 }, 'fast');
+    });
+    $("#closecreate").on("click",function(){
+        $("#htmlpage").fadeTo(500,1);
+        $("#navbar").fadeTo(500,1);
+        $("#navbarinfo").fadeTo(500,1);
+        $("#createpopupbox").fadeOut(function(){
+            $("#htmlpage").css("top","-8px");
+            $("#navbarinfo").css("top","-8px");
+            $("#htmlpage").css("z-index","0");
+            $("#navbar").css("z-index","1");
+            $("#navbarinfo").css("z-index","1");
+        });
+    });*/
+    //end of navbar js
+
+    $("#submitsupportingcontent").on("click",function(){
+        $("#uploadsupportingcontent").click();
+    });
+    
+    
+    
+    $("#uploadsupportingcontent").change(function(){
+        //check to see if file extensions are valid
+        var ext = $('#uploadbutton').val().split('.').pop().toLowerCase();
+			//var ext = filename.split('.').pop().toLowerCase();
+			var wrongext = false;
+			if($.inArray(ext, ['gif','png','jpg','jpeg','pptx','docx','xlsm']) == -1 ) {
+				alert("Sorry you can't upload files of this type :(");
+				wrongext = true;
+			}
+            //submitting and clicking php script when user uploaded correct ext
+            if(wrongext == false){
+				$("#confirmresource").click();
+			}
+        
+        
+        
+    });
+    $("#writenlecture").keyup(function(){
+        if($("#writenlecture").val()!==0){
+            $("#insertreading").hide();
+        }
+        if($("#writenlecture").val()==0){
+            $("#insertreading").show();    
+        }
+    });
+    
+    
+    
+    
+    });
